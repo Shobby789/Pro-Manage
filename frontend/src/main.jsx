1ee0ff485ea5cfcf6088bb2b9fe12b11d90c7053
Shobby789 <smshoaib2001@gmail.com>
2024-05-07 19:29:12 +0500

    resolved redux store error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,16 +5,14 @@ import "./index.css";
 // import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
 import { apiSlice } from "./redux/apiSlice/ApiSlice.js";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
 import { ApiProvider } from "@reduxjs/toolkit/query/react";
+import store from "./redux/store.js";
 // import { Pokemon } from "./features/Pokemon";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ApiProvider>
-        <App />
-      </ApiProvider>
+      <App />
     </Provider>
   </React.StrictMode>
 );
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,18 +1,24 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import { setupListeners } from "@reduxjs/toolkit/query";
+// import { apiSlice } from "./apiSlice/ApiSlice";
+
+// export const store = configureStore({
+//   reducer: {
+//     [apiSlice.reducerPath]: apiSlice.reducer,
+//   },
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware().concat(apiSlice.middleware),
+// });
+
 import { configureStore } from "@reduxjs/toolkit";
-import { setupListeners } from "@reduxjs/toolkit/query";
 import { apiSlice } from "./apiSlice/ApiSlice";
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
-    // Add the generated reducer as a specific top-level slice
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
-  // Adding the api middleware enables caching, invalidation, polling,
-  // and other useful features of `rtk-query`.
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(pokemonApi.middleware),
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
-// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
-// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
-setupListeners(store.dispatch);
+export default store;
